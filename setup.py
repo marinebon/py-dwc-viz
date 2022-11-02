38f5d12ce51215c54ec170f6440397f33e6d9af5
ayushanand18 <kingwarrior880@gmail.com>
2022-11-02 23:32:09 +0530

    update. setting up pydwcviz package

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     # The package metadata is specified in setup.cfg but GitHub's downstream dependency graph
     # does not work unless we put the name this here too.
-    name="dwcviz",
+    name="pydwcviz",
     use_scm_version={
-        "write_to": "dwcviz/_version.py",
+        "write_to": "pydwcviz/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
     },
