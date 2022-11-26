6ae6de817911179f126e7e38e02a320ca7f27ff4
ayushanand18 <kingwarrior880@gmail.com>
2022-11-26 18:48:35 +0530

    feat. added composition, qc, years, env, outliers function
    desc: add all functions to all the stats module from OBIS API

--- pydwcviz/stats/stats.py
+++ pydwcviz/stats/stats.py
@@ -68,28 +68,292 @@ def records(
     out = get(f'{obis_base_url}/statistics',args, **kwargs)
     return out
 
-def years():
+def years(
+    scientificname = None,
+    taxonid=None,
+    areaid=None,
+    datasetid=None,
+    nodeid=None,
+    startdate=None,
+    enddate=None,
+    startdepth=None,
+    enddepth=None,
+    geometry=None,
+    redlist=None,
+    hab=None,
+    wrims=None,
+    dropped=None,
+    absence=None,
+    flags=None,
+    exclude=None,
+    **kwargs,
+    ):
     """
     Get number of presence records per year.
     """
 
-def env():
+def env(
+    scientificname = None,
+    taxonid=None,
+    areaid=None,
+    datasetid=None,
+    nodeid=None,
+    startdate=None,
+    enddate=None,
+    startdepth=None,
+    enddepth=None,
+    geometry=None,
+    redlist=None,
+    hab=None,
+    wrims=None,
+    dropped=None,
+    absence=None,
+    flags=None,
+    exclude=None,
+    **kwargs,
+    ):
     """
     Get number of records per SST, SSS or depth bin.
-    """
 
-def qc():
+    :param scientificname: [string] Scientific name. Leave empty to include all taxa.
+    :param taxonid: [string] Taxon AphiaID.
+    :param areaid: [string] Area ID.
+    :param datasetid: [string] Dataset UUID.
+    :param nodeid: [string] Node UUID.
+    :param startdate: [string] Start date formatted as YYYY-MM-DD.
+    :param enddate: [string] End date formatted as YYYY-MM-DD.
+    :param startdepth: [integer] Start depth, in meters.
+    :param enddepth: [integer] End depth, in meters.
+    :param geometry: [string] Geometry, formatted as WKT or GeoHash.
+    :param redlist: [boolean] Red List species only, true/false.
+    :param hab: [boolean] HAB species only, true/false.
+    :param wrims: [boolean] WRiMS species only, true/false.
+    :param dropped: [string] Include dropped records (include) or get dropped records exclusively (true).
+    :param absence: [string] Include absence records (include) or get absence records exclusively (true).
+    :param flags: [string] Comma separated list of quality flags which need to be set.
+    :param exclude: [string] Comma separated list of quality flags to be excluded.
+
+    ::Usage
+
+    """
+    args = {
+        "scientificname":	scientificname,
+        "taxonid":	taxonid,
+        "areaid":	areaid,
+        "datasetid":	datasetid,
+        "nodeid":	nodeid,
+        "startdate":	startdate,
+        "enddate":	enddate,
+        "startdepth":	startdepth,
+        "enddepth":	enddepth,
+        "geometry":	geometry,
+        "redlist":	redlist,
+        "hab":	hab,
+        "wrims":	wrims,
+        "dropped":	dropped,
+        "absence":	absence,
+        "flags":	flags,
+        "exclude":	exclude,
+    }
+    out = get(f'{obis_base_url}/statistics',args, **kwargs)
+    return out
+
+def qc(
+    scientificname = None,
+    taxonid=None,
+    areaid=None,
+    datasetid=None,
+    nodeid=None,
+    startdate=None,
+    enddate=None,
+    startdepth=None,
+    enddepth=None,
+    geometry=None,
+    redlist=None,
+    hab=None,
+    wrims=None,
+    dropped=None,
+    absence=None,
+    flags=None,
+    exclude=None,
+    **kwargs,
+    ):
     """
     Get a QC summary, including missing or invalid values, number of records on land, 
     number of non marine records and number of records without Aphia ID.
-    """
+    
+    :param scientificname: [string] Scientific name. Leave empty to include all taxa.
+    :param taxonid: [string] Taxon AphiaID.
+    :param areaid: [string] Area ID.
+    :param datasetid: [string] Dataset UUID.
+    :param nodeid: [string] Node UUID.
+    :param startdate: [string] Start date formatted as YYYY-MM-DD.
+    :param enddate: [string] End date formatted as YYYY-MM-DD.
+    :param startdepth: [integer] Start depth, in meters.
+    :param enddepth: [integer] End depth, in meters.
+    :param geometry: [string] Geometry, formatted as WKT or GeoHash.
+    :param redlist: [boolean] Red List species only, true/false.
+    :param hab: [boolean] HAB species only, true/false.
+    :param wrims: [boolean] WRiMS species only, true/false.
+    :param dropped: [string] Include dropped records (include) or get dropped records exclusively (true).
+    :param absence: [string] Include absence records (include) or get absence records exclusively (true).
+    :param flags: [string] Comma separated list of quality flags which need to be set.
+    :param exclude: [string] Comma separated list of quality flags to be excluded.
 
-def composition():
+    ::Usage
+
+    """
+    args = {
+        "scientificname":	scientificname,
+        "taxonid":	taxonid,
+        "areaid":	areaid,
+        "datasetid":	datasetid,
+        "nodeid":	nodeid,
+        "startdate":	startdate,
+        "enddate":	enddate,
+        "startdepth":	startdepth,
+        "enddepth":	enddepth,
+        "geometry":	geometry,
+        "redlist":	redlist,
+        "hab":	hab,
+        "wrims":	wrims,
+        "dropped":	dropped,
+        "absence":	absence,
+        "flags":	flags,
+        "exclude":	exclude,
+    }
+    out = get(f'{obis_base_url}/statistics',args, **kwargs)
+    return out
+    
+
+def composition(
+    scientificname = None,
+    taxonid=None,
+    areaid=None,
+    datasetid=None,
+    nodeid=None,
+    startdate=None,
+    enddate=None,
+    startdepth=None,
+    enddepth=None,
+    geometry=None,
+    redlist=None,
+    hab=None,
+    wrims=None,
+    dropped=None,
+    absence=None,
+    flags=None,
+    exclude=None,
+    **kwargs,
+    ):
     """
     Get an overview of taxonomic composition.
-    """
+    
+    :param scientificname: [string] Scientific name. Leave empty to include all taxa.
+    :param taxonid: [string] Taxon AphiaID.
+    :param areaid: [string] Area ID.
+    :param datasetid: [string] Dataset UUID.
+    :param nodeid: [string] Node UUID.
+    :param startdate: [string] Start date formatted as YYYY-MM-DD.
+    :param enddate: [string] End date formatted as YYYY-MM-DD.
+    :param startdepth: [integer] Start depth, in meters.
+    :param enddepth: [integer] End depth, in meters.
+    :param geometry: [string] Geometry, formatted as WKT or GeoHash.
+    :param redlist: [boolean] Red List species only, true/false.
+    :param hab: [boolean] HAB species only, true/false.
+    :param wrims: [boolean] WRiMS species only, true/false.
+    :param dropped: [string] Include dropped records (include) or get dropped records exclusively (true).
+    :param absence: [string] Include absence records (include) or get absence records exclusively (true).
+    :param flags: [string] Comma separated list of quality flags which need to be set.
+    :param exclude: [string] Comma separated list of quality flags to be excluded.
 
-def outliers():
+    ::Usage
+
+    """
+    args = {
+        "scientificname":	scientificname,
+        "taxonid":	taxonid,
+        "areaid":	areaid,
+        "datasetid":	datasetid,
+        "nodeid":	nodeid,
+        "startdate":	startdate,
+        "enddate":	enddate,
+        "startdepth":	startdepth,
+        "enddepth":	enddepth,
+        "geometry":	geometry,
+        "redlist":	redlist,
+        "hab":	hab,
+        "wrims":	wrims,
+        "dropped":	dropped,
+        "absence":	absence,
+        "flags":	flags,
+        "exclude":	exclude,
+    }
+    out = get(f'{obis_base_url}/statistics',args, **kwargs)
+    return out
+
+def outliers(
+    scientificname = None,
+    taxonid=None,
+    areaid=None,
+    datasetid=None,
+    nodeid=None,
+    startdate=None,
+    enddate=None,
+    startdepth=None,
+    enddepth=None,
+    geometry=None,
+    redlist=None,
+    hab=None,
+    wrims=None,
+    dropped=None,
+    absence=None,
+    flags=None,
+    exclude=None,
+    **kwargs,
+    ):
     """
     Get SST and SSS distribution quartiles.
+    
+    :param scientificname: [string] Scientific name. Leave empty to include all taxa.
+    :param taxonid: [string] Taxon AphiaID.
+    :param areaid: [string] Area ID.
+    :param datasetid: [string] Dataset UUID.
+    :param nodeid: [string] Node UUID.
+    :param startdate: [string] Start date formatted as YYYY-MM-DD.
+    :param enddate: [string] End date formatted as YYYY-MM-DD.
+    :param startdepth: [integer] Start depth, in meters.
+    :param enddepth: [integer] End depth, in meters.
+    :param geometry: [string] Geometry, formatted as WKT or GeoHash.
+    :param redlist: [boolean] Red List species only, true/false.
+    :param hab: [boolean] HAB species only, true/false.
+    :param wrims: [boolean] WRiMS species only, true/false.
+    :param dropped: [string] Include dropped records (include) or get dropped records exclusively (true).
+    :param absence: [string] Include absence records (include) or get absence records exclusively (true).
+    :param flags: [string] Comma separated list of quality flags which need to be set.
+    :param exclude: [string] Comma separated list of quality flags to be excluded.
+
+    ::Usage
+
     """
+    args = {
+        "scientificname":	scientificname,
+        "taxonid":	taxonid,
+        "areaid":	areaid,
+        "datasetid":	datasetid,
+        "nodeid":	nodeid,
+        "startdate":	startdate,
+        "enddate":	enddate,
+        "startdepth":	startdepth,
+        "enddepth":	enddepth,
+        "geometry":	geometry,
+        "redlist":	redlist,
+        "hab":	hab,
+        "wrims":	wrims,
+        "dropped":	dropped,
+        "absence":	absence,
+        "flags":	flags,
+        "exclude":	exclude,
+    }
+    out = get(f'{obis_base_url}/statistics',args, **kwargs)
+    return out
