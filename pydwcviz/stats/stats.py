c7a2fcfe3dd54ec472ec852482bb3d85ecc20127
ayushanand18 <kingwarrior880@gmail.com>
2022-11-26 19:09:36 +0530

    update. updated get_env function

--- pydwcviz/stats/stats.py
+++ pydwcviz/stats/stats.py
@@ -90,7 +90,49 @@ def get_years(
     ):
     """
     Get number of presence records per year.
+
+    :param scientificname: [string] Scientific name. Leave empty to include all taxa.
+    :param taxonid: [string] Taxon AphiaID.
+    :param areaid: [string] Area ID.
+    :param datasetid: [string] Dataset UUID.
+    :param nodeid: [string] Node UUID.
+    :param startdate: [string] Start date formatted as YYYY-MM-DD.
+    :param enddate: [string] End date formatted as YYYY-MM-DD.
+    :param startdepth: [integer] Start depth, in meters.
+    :param enddepth: [integer] End depth, in meters.
+    :param geometry: [string] Geometry, formatted as WKT or GeoHash.
+    :param redlist: [boolean] Red List species only, true/false.
+    :param hab: [boolean] HAB species only, true/false.
+    :param wrims: [boolean] WRiMS species only, true/false.
+    :param dropped: [string] Include dropped records (include) or get dropped records exclusively (true).
+    :param absence: [string] Include absence records (include) or get absence records exclusively (true).
+    :param flags: [string] Comma separated list of quality flags which need to be set.
+    :param exclude: [string] Comma separated list of quality flags to be excluded.
+
+    ::Usage
+
     """
+    args = {
+        "scientificname":	scientificname,
+        "taxonid":	taxonid,
+        "areaid":	areaid,
+        "datasetid":	datasetid,
+        "nodeid":	nodeid,
+        "startdate":	startdate,
+        "enddate":	enddate,
+        "startdepth":	startdepth,
+        "enddepth":	enddepth,
+        "geometry":	geometry,
+        "redlist":	redlist,
+        "hab":	hab,
+        "wrims":	wrims,
+        "dropped":	dropped,
+        "absence":	absence,
+        "flags":	flags,
+        "exclude":	exclude,
+    }
+    out = get(f'{obis_base_url}/statistics',args, **kwargs)
+    return out
 
 def get_env(
     scientificname = None,
