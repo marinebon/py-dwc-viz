7703e83a5e2526da7307bc20848369faa1462003
ayushanand18 <kingwarrior880@gmail.com>
2022-12-22 22:45:45 +0530

    fix failing test and adding comments

--- pydwcviz/diversity/diversity.py
+++ pydwcviz/diversity/diversity.py
@@ -24,23 +24,25 @@ def shannon(df, decimals=3):
         
         diversity.shannon(data, 3)
     """
+    # if the data source is GBIF then convert it to compatible format (column headings)
     if "id" not in df.columns:
         df.rename({"gbifID":"id"}, inplace=True)
     
-    
-    _df = df.dropna(subset=["species"])
-        .round(
-            {"decimalLongitude":decimals, "decimalLatitude":decimals}
+    # prepare index only for species and drop unnecessary columns
+    _df = df.dropna(subset=["species"]).round(
+        {"decimalLongitude":decimals, "decimalLatitude":decimals}
         )[["decimalLongitude","decimalLatitude","species","id"]]
-    print(f"{len(df.index) - len(_df.index)} NA-containing records dropped.")
+    print(f"{len(df.index) - len(_df.index)} Not species records dropped.")
     
+    # aggregate number of records as a proxy for estimating abundance
     _sh_df = pd.DataFrame(
         _df.groupby(["decimalLongitude", "decimalLatitude", "species"]).id.count()
     )
-    print(f"{len(_sh_df.index)} unique species*locations found.")
+    print(f"{len(_sh_df.index)} unique species*locations records found.")
     
-    # calculate the index
+    # preparing the Pi for each species*location pair and calculating individual coefficients
     _sh_df["sum"] = _df.groupby(["decimalLongitude", "decimalLatitude"]).id.count()
     _sh_df["coeff"] = _sh_df["id"] / _sh_df["sum"] * (_sh_df["id"] / _sh_df["sum"]).apply(math.log)
     
+    # sum up coefficients for all species in a location and calculate the total biodiversity
     return _sh_df.reset_index().groupby(["decimalLongitude", "decimalLatitude"]).coeff.sum().reset_index()
