96183481ab42070af9bead6387c8e5c55b8a037b
Tylar <email@tylar.info>
2022-12-22 11:29:17 -0500

    + more verbose formatting + printouts
    helps my readability.

--- pydwcviz/diversity/diversity.py
+++ pydwcviz/diversity/diversity.py
@@ -18,7 +18,8 @@ def shannon(df, decimals=3):
         from pydwcviz import diversity
 
         data = occurrences.search(
-            geometry="POLYGON ((58.3301 19.0935, 69.8145 19.0381, 69.8145 9.5161, 58.6230 9.6316, 58.3301 19.0935))", # this is a geometry in the Arabian Sea (right of India)
+            # this is a geometry in the Arabian Sea (right of India)
+            geometry="POLYGON ((58.3301 19.0935, 69.8145 19.0381, 69.8145 9.5161, 58.6230 9.6316, 58.3301 19.0935))", 
         ).execute()
         
         diversity.shannon(data, 3)
@@ -26,8 +27,20 @@ def shannon(df, decimals=3):
     if "id" not in df.columns:
         df.rename({"gbifID":"id"}, inplace=True)
     
-    _df = df.dropna(subset=["species"]).round({"decimalLongitude":decimals, "decimalLatitude":decimals})[["decimalLongitude","decimalLatitude","species","id"]]
-    _sh_df = pd.DataFrame(_df.groupby(["decimalLongitude", "decimalLatitude", "species"]).id.count())
+    
+    _df = df.dropna(subset=["species"])
+        .round(
+            {"decimalLongitude":decimals, "decimalLatitude":decimals}
+        )[["decimalLongitude","decimalLatitude","species","id"]]
+    print(f"{len(df.index) - len(_df.index)} NA-containing records dropped.")
+    
+    _sh_df = pd.DataFrame(
+        _df.groupby(["decimalLongitude", "decimalLatitude", "species"]).id.count()
+    )
+    print(f"{len(_sh_df.index)} unique species*locations found.")
+    
+    # calculate the index
     _sh_df["sum"] = _df.groupby(["decimalLongitude", "decimalLatitude"]).id.count()
-    _sh_df["coeff"] = _sh_df["id"]/_sh_df["sum"]*(_sh_df["id"]/_sh_df["sum"]).apply(math.log)
+    _sh_df["coeff"] = _sh_df["id"] / _sh_df["sum"] * (_sh_df["id"] / _sh_df["sum"]).apply(math.log)
+    
     return _sh_df.reset_index().groupby(["decimalLongitude", "decimalLatitude"]).coeff.sum().reset_index()
